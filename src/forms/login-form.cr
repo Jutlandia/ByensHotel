3e22d2961acc72addd979ada3c925a282e64ddde
Henrik Christensen <sensen1695@hotmail.com>
2021-10-27 20:55:43 +0200

    login and register routes + forms

---
+++ src/forms/login-form.cr
@@ -0,0 +1,16 @@
+class LoginForm < Kemal::Form
+  field username : Kemal::Form::TextField,
+                     attrs: {"class" => "input"},
+                     label: Kemal::Form::Label.new(
+                      "username",
+                      "Username",
+                      {"class" => "label"}),
+                     validators: [Kemal::FormValidator::Required.new]
+  field password : Kemal::Form::PasswordField,
+                     attrs: {"class" => "input"},
+                     label: Kemal::Form::Label.new(
+                      "password",
+                      "Password",
+                      {"class" => "label"}),
+                     validators: [Kemal::FormValidator::Required.new]
+end
--- src/jutlandia.cr
+++ src/jutlandia.cr
@@ -2,6 +2,8 @@ require "dotenv"
 require "kemal"
 require "kemal-session"
 require "kemal-csrf"
+require "kemal-form"
+require "./**"
 
 if Kemal.config.env == "development"
   Dotenv.load
@@ -17,6 +19,40 @@ add_handler CSRF.new
 get "/" do
   page_title = "Home"
   render "src/views/index.ecr", "src/views/layouts/layout.ecr"
+end
+
+get "/login" do |env|
+  page_title = "Log in"
+  form = LoginForm.new
+  render "src/views/auth/login.ecr", "src/views/layouts/layout.ecr"
+end
+
+post "/login" do |env|
+  page_title = "Log in"
+  form = LoginForm.new env
+  if form.valid?
+    # TODO: verify that the credentials are correct
+    env.redirect "/"
+    next
+  end
+  render "src/views/auth/login.ecr", "src/views/layouts/layout.ecr"
+end
+
+get "/register" do |env|
+  page_title = "Register"
+  form = RegisterForm.new
+  render "src/views/auth/register.ecr", "src/views/layouts/layout.ecr"
+end
+
+post "/register" do |env|
+  page_title = "Register"
+  form = RegisterForm.new env
+  if form.valid?
+    # TODO: do stuff
+    env.redirect "/login"
+    next
+  end
+  render "src/views/auth/register.ecr", "src/views/layouts/layout.ecr"
 end
 
 Kemal.run
