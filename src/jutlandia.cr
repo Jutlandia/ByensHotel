c762867af5411cecc1029f10d4a0b6caa0e73f41
Henrik Christensen <sensen1695@hotmail.com>
2021-10-27 11:08:39 +0200

    setup kemal app

--- src/jutlandia.cr
+++ src/jutlandia.cr
@@ -1,6 +1,22 @@
-# TODO: Write documentation for `Jutlandia`
-module Jutlandia
-  VERSION = "0.1.0"
+require "dotenv"
+require "kemal"
+require "kemal-session"
+require "kemal-csrf"
 
-  # TODO: Put your code here
+if Kemal.config.env == "development"
+  Dotenv.load
 end
+
+Kemal::Session.config do |config|
+  config.secret = ENV["SESSION_SECRET"]
+  config.secure = Kemal.config.env == "production"
+end
+
+add_handler CSRF.new
+
+get "/" do
+  page_title = "Home"
+  render "src/views/index.ecr", "src/views/layouts/layout.ecr"
+end
+
+Kemal.run
