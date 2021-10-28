ccbc2f88d0ec794948739204895cf397e1584469
Henrik Christensen <sensen1695@hotmail.com>
2021-10-28 23:54:39 +0200

    added github action + crystal tool format

--- src/forms/register-form.cr
+++ src/forms/register-form.cr
@@ -2,36 +2,36 @@ require "kemal-form"
 
 class RegisterForm < Kemal::Form
   field username : Kemal::Form::TextField,
-                     attrs: {"class" => "input"},
-                     label: Kemal::Form::Label.new(
-                      "username",
-                      "Username",
-                      {"class" => "label"}),
-                     validators: [Kemal::FormValidator::Required.new]
+    attrs: {"class" => "input"},
+    label: Kemal::Form::Label.new(
+      "username",
+      "Username",
+      {"class" => "label"}),
+    validators: [Kemal::FormValidator::Required.new]
   field email : Kemal::Form::EmailField,
-                  attrs: {"class" => "input"},
-                  label: Kemal::Form::Label.new(
-                   "email",
-                   "Email",
-                   {"class" => "label"}),
-                  validators: [
-                    Kemal::FormValidator::Required.new,
-                    Kemal::FormValidator::Email.new
-                  ]
+    attrs: {"class" => "input"},
+    label: Kemal::Form::Label.new(
+      "email",
+      "Email",
+      {"class" => "label"}),
+    validators: [
+      Kemal::FormValidator::Required.new,
+      Kemal::FormValidator::Email.new,
+    ]
   field password : Kemal::Form::PasswordField,
-                     attrs: {"class" => "input"},
-                     label: Kemal::Form::Label.new(
-                      "password",
-                      "Password",
-                      {"class" => "label"}),
-                     validators: [Kemal::FormValidator::Required.new]
+    attrs: {"class" => "input"},
+    label: Kemal::Form::Label.new(
+      "password",
+      "Password",
+      {"class" => "label"}),
+    validators: [Kemal::FormValidator::Required.new]
   field confirm_password : Kemal::Form::PasswordField,
-                     attrs: {"class" => "input"},
-                     label: Kemal::Form::Label.new(
-                      "confirm_password",
-                      "Confirm Password",
-                      {"class" => "label"}),
-                     validators: [Kemal::FormValidator::Required.new]
+    attrs: {"class" => "input"},
+    label: Kemal::Form::Label.new(
+      "confirm_password",
+      "Confirm Password",
+      {"class" => "label"}),
+    validators: [Kemal::FormValidator::Required.new]
 
   def valid?
     if @password.value != @confirm_password.value
